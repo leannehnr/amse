2a49028d9973009f87615f27ee739f7d81561186
leannehnr <leanne.henry80@gmail.com>
2025-02-05 15:59:28 +0100

    navigation onglet clean

--- tp1/lib/main.dart
+++ tp1/lib/main.dart
@@ -1,65 +1,17 @@
 import 'package:flutter/material.dart';
-import 'home_page.dart'; // Page d'accueil
-import 'dico_page.dart'; // Page de génération de recettes
-import 'favorites_page.dart'; // Page des favoris
+import 'main_page.dart'; // Page d'accueil
 
-class MainPage extends StatefulWidget {
-  @override
-  MainPageState createState() => MainPageState();
+void main(){
+  runApp(MyApp());
 }
 
-class MainPageState extends State<MainPage> {
-  int _selectedIndex = 0; // Index de la page active
-
-  final List<Widget> _pages = [
-    HomePage(),       // Page Accueil
-    GeneratorPage(),  // Page Générateur
-    FavoritesPage(),  // Page Favoris
-  ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
+class MyApp extends StatelessWidget{
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: _pages[_selectedIndex], // Affiche la page sélectionnée
-
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _selectedIndex,
-        onTap: _onItemTapped,
-        backgroundColor: Colors.black,
-        selectedItemColor: Color(0xFFD8C3A5), // Beige
-        unselectedItemColor: Colors.grey,
-        showSelectedLabels: false,
-        showUnselectedLabels: false,
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: "Accueil",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.folder), // Icône pour le générateur de recettes
-            label: "Générateur",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.favorite),
-            label: "Favoris",
-          ),
-        ],
-      ),
-
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Color(0xFFD8C3A5),
-        child: Icon(Icons.add, color: Colors.black),
-        onPressed: () {
-          // Action pour ajouter une recette
-        },
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+  Widget build(BuildContext context){
+    return MaterialApp(debugShowCheckedModeBanner: false,
+    title: 'Gestion de recettes', 
+    theme: ThemeData(colorScheme: ColorScheme.fromSeed(seedColor: Color(0xFFD8C3A5)),),
+    home: MainPage(),
     );
   }
 }
---
+++ tp1/lib/main_page.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'home_page.dart'; // Page d'accueil
+import 'dico_page.dart'; // Page de génération de recettes
+import 'favorites_page.dart'; // Page des favoris
+
+class MainPage extends StatefulWidget {
+  @override
+  MainPageState createState() => MainPageState();
+}
+
+class MainPageState extends State<MainPage> {
+  int _selectedIndex = 0; // Index de la page active
+
+  final List<Widget> _pages = [
+    HomePage(),       // Page Accueil
+    GeneratorPage(),  // Page Générateur
+    FavoritesPage(),  // Page Favoris
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pages[_selectedIndex], // Affiche la page sélectionnée
+
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        backgroundColor: Colors.black,
+        selectedItemColor: Color(0xFFD8C3A5), // Beige
+        unselectedItemColor: Colors.grey,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Accueil",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.folder), // Icône pour le générateur de recettes
+            label: "Générateur",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: "Favoris",
+          ),
+        ],
+      ),
+    );
+  }
+}
