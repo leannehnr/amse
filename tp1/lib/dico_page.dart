b77a5cde7b9af347d6cee6dc0a3daf0c50a6c5e4
leannehnr <leanne.henry80@gmail.com>
2025-02-05 18:58:08 +0100

    voir comment mettre toutes les recettes dans l'app

--- tp1/lib/dico_page.dart
+++ tp1/lib/dico_page.dart
@@ -1,8 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart' show rootBundle;
+import 'recette_model.dart';
 
+
+Future<List<Recette>> loadRecettes() async {
+  final String response = await rootBundle.loadString('assets/recettes.json');
+  final data = json.decode(response);
+  List<dynamic> recettesJson = data["recettes"];
+  return recettesJson.map((json) => Recette.fromJson(json)).toList();
+}
+
+// Passer en stateful pour pouvoir intéragir avec les recettes ??
 class GeneratorPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    
     return Center(
       child: Text("Page Générateur de Recettes", style: TextStyle(fontSize: 20)),
     );
---
+++ tp1/lib/recette_model.dart
@@ -0,0 +1,42 @@
+import 'dart:convert';
+
+class Recette{
+final int id;
+  final String nom;
+  final String type;
+  final String regime;
+  final String tempsPreparation;
+  final String tempsCuisson;
+  final List<String> ingredients;
+  final List<String> etapes;
+  final String image;
+  final bool favori;
+
+  Recette({
+    required this.id,
+    required this.nom,
+    required this.type,
+    required this.regime,
+    required this.tempsPreparation,
+    required this.tempsCuisson,
+    required this.ingredients,
+    required this.etapes,
+    required this.image,
+    required this.favori,
+  });
+
+  factory Recette.fromJson(Map<String, dynamic> json) {
+    return Recette(
+      id: json['id'],
+      nom: json['nom'],
+      type: json['type'],
+      regime: json['regime'],
+      tempsPreparation: json['temps_preparation'],
+      tempsCuisson: json['temps_cuisson'],
+      ingredients: List<String>.from(json['ingredients']),
+      etapes: List<String>.from(json['etapes']),
+      image: json['image'],
+      favori: json['favori'],
+    );
+  }
+}
